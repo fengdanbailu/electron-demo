e3d57195f3272277ce282a802d0054d7ec350bc4
guoming chen <chenguoming2015@163.com>
2022-05-12 00:26:48 +0800

    使用最新版本的electron

--- main.js
+++ main.js
@@ -1,49 +1,45 @@
-/*
- * @Author: gm.chen
- * @Date: 2020-07-29 13:22:31
- * @LastEditors: gm.chen
- * @LastEditTime: 2020-08-04 09:10:31
- * @Description: file content
- * @FilePath: /electron-demo/main.js
- */ 
-const { app, BrowserWindow } = require('electron')
+// main.js
 
-function createWindow () {   
-  // 创建浏览器窗口
-  const win = new BrowserWindow({
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  // 并且为你的应用加载index.html
-  win.loadFile('index.html')
+  // 加载 index.html
+  mainWindow.loadFile('index.html')
 
-  // 打开开发者工具
-  if (process.env.NODE_ENV === 'development') {
-    win.webContents.openDevTools();
-  }
+  // 打开开发工具
+  // mainWindow.webContents.openDevTools()
 }
 
-// Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
+// 这段程序将会在 Electron 结束初始化
+// 和创建浏览器窗口的时候调用
 // 部分 API 在 ready 事件触发后才能使用。
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow()
 
-//当所有窗口都被关闭后退出
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('activate', () => {
-  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-  // 通常在应用程序中重新创建一个窗口。
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+// In this file you can include the rest of your app's specific main process
+// code. 也可以拆分成几个文件，然后用 require 导入。
